a567c6e023a8dafbc1b0e79e9515722f452bfe72
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-05-03 00:15:29 +0300

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '1000',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Larvender Drone',
+        productNumber: '1001',
+        paymentStatus: 'Refunded',
+        shipping: 'declined'
+    },
+    {
+        productName: 'Ruko F11 pro Drone',
+        productNumber: '1003',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'camera drone',
+        productNumber: '1004',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'GPS drone',
+        productNumber: '1005',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'DJI air drone',
+        productNumber: '1006',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'Lozenge drone',
+        productNumber: '1007',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    }
+]
